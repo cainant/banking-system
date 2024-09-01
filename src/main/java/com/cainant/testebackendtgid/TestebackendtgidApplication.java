33dd043a5b6acc8cceb052ce29fa38df30c0498b
ctravizanutto <ctravizanutto@gmail.com>
2024-09-01 18:10:44 -0300

    dont know why those were unversioned

---
+++ src/main/java/com/cainant/testebackendtgid/TestebackendtgidApplication.java
@@ -0,0 +1,13 @@
+package com.cainant.testebackendtgid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestebackendtgidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestebackendtgidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cainant/testebackendtgid/TestebackendtgidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cainant.testebackendtgid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestebackendtgidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
